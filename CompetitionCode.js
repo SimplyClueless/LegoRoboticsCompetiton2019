7b12cd41a9131697443206943ddc4f3614de0f06
SimplyClueless <39248934+SimplyClueless@users.noreply.github.com>
2020-09-13 18:19:25 +1000

    Update CompetitionCode.js
    fixed variable names

--- CompetitionCode.js
+++ CompetitionCode.js
@@ -1,27 +1,27 @@
 //===============[ Global Variables ]===============\\
-const LeftSensor = sensors.color1;
-const RightSensor = sensors.color2;
-const UltraSensor = sensors.ultrasonic4;
-const MainMotors = motors.largeAB;
-const ClawMotor = motors.mediumC;
+const leftSensor = sensors.color1;
+const rightSensor = sensors.color2;
+const ultraSensor = sensors.ultrasonic4;
+const mainMotors = motors.largeAB;
+const clawMotor = motors.mediumC;
 const GREEN = ColorSensorColor.Green;
 const BLUE = ColorSensorColor.Blue;
 const SHARPNESS = 3;
-const Speed = 30;
-let WaterTime = 0;
-let ForwardTime = 0;
-let TurningTime = 0;
-let FirstRun = true;
-let ChemicalSpillRunning = false;
-let Running = "Moving";
+const SPEED = 30;
+let waterTime = 0;
+let forwardTime = 0;
+let turningTime = 0;
+let firstRun = true;
+let chemicalSpillRunning = false;
+let running = "Moving";
 
 //===============[ Initialization & Looping Functions ]===============\\
 loops.forever(function () {
-    if (FirstRun == true) {
+    if (firstRun == true) {
         brick.showString("Initialising...", 1);
         if (GetLeftReflected() != 0) {
             brick.showString("Sensors Active!", 1);
-            FirstRun = false;
+            firstRun = false;
         }
     }
     else {
@@ -29,41 +29,42 @@ loops.forever(function () {
         Intersection();
         WaterTower();
         PrintValues();
-        if (WaterTime != 100) {
-            WaterTime += 1;
+        if (waterTime != 100) {
+            waterTime += 1;
         }
     }
 })
 
 //===============[ Primary Functions ] ===============\\
 function MoveRobot() {
-    if (ChemicalSpillRunning == false) {
+    if (chemicalSpillRunning == false) {
         brick.setStatusLight(StatusLight.Green);
-        Running = "Moving...";
-        MainMotors.steer(GetDirection(), Speed);
+        running = "Moving...";
+        mainMotors.steer(GetDirection(), SPEED);
     }
 }
 
 function Intersection() {
-    if (ChemicalSpillRunning == false) {
+    if (
+        hemicalSpillRunning == false) {
         if (GetLeftReflected() < 30 && GetRightReflected() < 30) {
             brick.setStatusLight(StatusLight.Orange);
-            Running = "T Intersection...";
-            MainMotors.tank(10, 10, 0.75, MoveUnit.Seconds);
+            running = "T Intersection...";
+            mainMotors.tank(10, 10, 0.75, MoveUnit.Seconds);
             motors.stopAll();
             pause(100);
-            if ((GetLeftColor() == GREEN || GetLeftColor() == BLUE) && (GetRightColor() == GREEN || GetRightColor() == BLUE) && ChemicalSpillRunning == false) {
+            if ((GetLeftColor() == GREEN || GetLeftColor() == BLUE) && (GetRightColor() == GREEN || GetRightColor() == BLUE) && chemicalSpillRunning == false) {
                 ChemicalSpill();
             }
             else if (GetLeftColor() == GREEN) {
                 brick.setStatusLight(StatusLight.OrangeFlash);
-                Running = "Turning Left...";
-                MainMotors.tank(10, 20, .5, MoveUnit.Seconds);
+                running = "Turning Left...";
+                mainMotors.tank(10, 20, .5, MoveUnit.Seconds);
             }
             else if (GetRightColor() == GREEN) {
                 brick.setStatusLight(StatusLight.OrangeFlash);
-                Running = "Turning Right...";
-                MainMotors.tank(20, 10, .5, MoveUnit.Seconds);
+                running = "Turning Right...";
+                mainMotors.tank(20, 10, .5, MoveUnit.Seconds);
             }
             else {
                 return;
@@ -73,12 +74,12 @@ function Intersection() {
 }
 
 function ChemicalSpill() {
-    if (ChemicalSpillRunning == false) {
-        ChemicalSpillRunning = true;
+    if (chemicalSpillRunning == false) {
+        chemicalSpillRunning = true;
         brick.setStatusLight(StatusLight.RedFlash);
-        Running = "Entered Chemical Spill..."
-        MainMotors.tank(20, -10, 1.5, MoveUnit.Seconds)
-        ClawMotor.run(25, 2, MoveUnit.Seconds);
+        running = "Entered Chemical Spill..."
+        mainMotors.tank(20, -10, 1.5, MoveUnit.Seconds)
+        clawMotor.run(25, 2, MoveUnit.Seconds);
         VictimRescue();
     }
     else {
@@ -89,94 +90,94 @@ function ChemicalSpill() {
 function VictimRescue() {
     loops.forever(function () {
         brick.setStatusLight(StatusLight.RedFlash);
-        Running = "Locating Can...";
+        running = "Locating Can...";
         PrintValues();
-        if (UltraSensor.distance() > 8 && UltraSensor.distance() < 40) {
-            MainMotors.tank(10, 10, 1, MoveUnit.Seconds);
-            ForwardTime += 1;
+        if (ultraSensor.distance() > 8 && ultraSensor.distance() < 40) {
+            mainMotors.tank(10, 10, 1, MoveUnit.Seconds);
+            forwardTime += 1;
         }
-        else if (UltraSensor.distance() < 8) {
+        else if (ultraSensor.distance() < 8) {
             brick.setStatusLight(StatusLight.GreenFlash);
-            Running = "Collecting Can..."
-            MainMotors.tank(-5, 5, .5, MoveUnit.Seconds);
-            MainMotors.tank(10, 10, 1, MoveUnit.Seconds);
-            MainMotors.tank(5, -5, .5, MoveUnit.Seconds);
-            MainMotors.tank(10, 10, 1, MoveUnit.Seconds);
-            ClawMotor.run(-15, 3, MoveUnit.Seconds);
-            MainMotors.tank(-10, -10, 1, MoveUnit.Seconds);
-            MainMotors.tank(-5, 5, .5, MoveUnit.Seconds);
-            MainMotors.tank(-10, -10, 1, MoveUnit.Seconds);
-            MainMotors.tank(5, -5, .5, MoveUnit.Seconds);
+            running = "Collecting Can..."
+            mainMotors.tank(-5, 5, .5, MoveUnit.Seconds);
+            mainMotors.tank(10, 10, 1, MoveUnit.Seconds);
+            mainMotors.tank(5, -5, .5, MoveUnit.Seconds);
+            mainMotors.tank(10, 10, 1, MoveUnit.Seconds);
+            clawMotor.run(-15, 3, MoveUnit.Seconds);
+            mainMotors.tank(-10, -10, 1, MoveUnit.Seconds);
+            mainMotors.tank(-5, 5, .5, MoveUnit.Seconds);
+            mainMotors.tank(-10, -10, 1, MoveUnit.Seconds);
+            mainMotors.tank(5, -5, .5, MoveUnit.Seconds);
             ExitChemicalSpill();
             return;
         }
         else {
-            MainMotors.tank(-5, 5, 1, MoveUnit.Seconds);
-            TurningTime += 1;
+            mainMotors.tank(-5, 5, 1, MoveUnit.Seconds);
+            turningTime += 1;
         }
     })
 }
 
 function ExitChemicalSpill() {
     brick.setStatusLight(StatusLight.GreenFlash);
-    Running = "Exiting Chemical Spill..."
-    MainMotors.tank(-10, -10, ForwardTime, MoveUnit.Seconds);
-    MainMotors.tank(5, -5, TurningTime, MoveUnit.Seconds);
-    MainMotors.tank(5, 5, 2, MoveUnit.Seconds);
-    MainMotors.tank(5, -5, 2, MoveUnit.Seconds);
-    MainMotors.tank(5, 5, 2, MoveUnit.Seconds)
-    ClawMotor.run(15, 3, MoveUnit.Seconds);
-    MainMotors.tank(-5, -5, 2, MoveUnit.Seconds);
-    ClawMotor.run(-25, 2, MoveUnit.Seconds);
-    MainMotors.tank(-5, -5, 2, MoveUnit.Seconds);
-    MainMotors.tank(20, -10, 2.75, MoveUnit.Seconds);
-    ChemicalSpillRunning = false;
+    running = "Exiting Chemical Spill..."
+    mainMotors.tank(-10, -10, forwardTime, MoveUnit.Seconds);
+    mainMotors.tank(5, -5, turningTime, MoveUnit.Seconds);
+    mainMotors.tank(5, 5, 2, MoveUnit.Seconds);
+    mainMotors.tank(5, -5, 2, MoveUnit.Seconds);
+    mainMotors.tank(5, 5, 2, MoveUnit.Seconds)
+    clawMotor.run(15, 3, MoveUnit.Seconds);
+    mainMotors.tank(-5, -5, 2, MoveUnit.Seconds);
+    clawMotor.run(-25, 2, MoveUnit.Seconds);
+    mainMotors.tank(-5, -5, 2, MoveUnit.Seconds);
+    mainMotors.tank(20, -10, 2.75, MoveUnit.Seconds);
+    chemicalSpillRunning = false;
 }
 
 function WaterTower() {
-    if (WaterTime >= 100 && ChemicalSpillRunning == false) {
-        if (UltraSensor.distance() <= 9 && UltraSensor.distance() > 7.75) {
+    if (waterTime >= 100 && chemicalSpillRunning == false) {
+        if (ultraSensor.distance() <= 9 && ultraSensor.distance() > 7.75) {
             brick.setStatusLight(StatusLight.OrangeFlash);
-            Running = "Water Tower...";
-            MainMotors.steer(GetDirection() * -1, -10, 1, MoveUnit.Seconds);
-            MainMotors.tank(0, 10, 3.2, MoveUnit.Seconds);
-            MainMotors.tank(25, 25, .6, MoveUnit.Seconds);
-            MainMotors.tank(20, 0, 1.6, MoveUnit.Seconds);
-            MainMotors.tank(25, 25, 1.75, MoveUnit.Seconds);
-            MainMotors.tank(20, 0, 1.5, MoveUnit.Seconds);
+            running = "Water Tower...";
+            mainMotors.steer(GetDirection() * -1, -10, 1, MoveUnit.Seconds);
+            mainMotors.tank(0, 10, 3.2, MoveUnit.Seconds);
+            mainMotors.tank(25, 25, .6, MoveUnit.Seconds);
+            mainMotors.tank(20, 0, 1.6, MoveUnit.Seconds);
+            mainMotors.tank(25, 25, 1.75, MoveUnit.Seconds);
+            mainMotors.tank(20, 0, 1.5, MoveUnit.Seconds);
             while (GetRightReflected() > 60) {
-                MainMotors.tank(10, 10, .5, MoveUnit.Seconds);
+                mainMotors.tank(10, 10, .5, MoveUnit.Seconds);
             }
-            MainMotors.tank(0, 20, 1.65, MoveUnit.Seconds);
+            ainMotors.tank(0, 20, 1.65, MoveUnit.Seconds);
         }
     }
 }
 
 //===============[ Helper Functions ]===============\\
 function GetLeftReflected() {
-    return LeftSensor.light(LightIntensityMode.Reflected);
+    return leftSensor.light(LightIntensityMode.Reflected);
 }
 
 function GetRightReflected() {
-    return RightSensor.light(LightIntensityMode.Reflected);
+    return rightSensor.light(LightIntensityMode.Reflected);
 }
 
 function GetLeftColor() {
-    LeftSensor.color();
+    leftSensor.color();
     pause(50);
-    const LeftColor = LeftSensor.color();
+    const leftColor = leftSensor.color();
     GetLeftReflected();
     pause(50);
-    return LeftColor;
+    return leftColor;
 }
 
 function GetRightColor() {
-    RightSensor.color();
+    rightSensor.color();
     pause(50);
-    const RightColor = RightSensor.color();
+    const rightColor = rightSensor.color();
     GetRightReflected();
     pause(50);
-    return RightColor;
+    return rightColor;
 }
 
 function GetSensorDifference() {
@@ -190,9 +191,9 @@ function GetDirection() {
 
 function PrintValues() {
     brick.showString("Status: ", 1);
-    brick.showString(Running, 2);
+    brick.showString(running, 2);
     brick.showValue("LeftSensor", GetLeftReflected(), 4);
     brick.showValue("RightSensor", GetRightReflected(), 5);
     brick.showValue("Direction", GetDirection(), 6);
-    brick.showValue("UltraSensor", UltraSensor.distance(), 7);
+    brick.showValue("UltraSensor", ultraSensor.distance(), 7);
 }
